974602eab390a54b18a114ede96b5de96cec238c
Vytautas Valutis <dnc908@gmail.com>
2023-02-07 22:54:45 +0200

    First JS script in HTML

---
+++ js/data/ourProductNavData.js
@@ -0,0 +1,22 @@
+const ourProductNavData = [
+    {
+        name: 'All',
+    },
+    {
+        name: 'Business',
+    },
+    {
+        name: 'Design',
+    },
+    {
+        name: 'Technology',
+    },
+    {
+        name: 'Development',
+    },
+    {
+        name: 'Agency',
+    },
+];
+
+export { ourProductNavData };
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,9 +1,23 @@
 // IMPORT
 
+import { ourProductNavData } from '../data/ourProductNavData.js';
+
 // EXECUTION
 
 /* header: start */
 /* header: end */
+
+/* our project: start */
+const ourProductListDOM = document.getElementById('our_product');
+const itemOpDOM = ourProductListDOM.querySelectorAll('.ourPr');
+console.log(itemOpDOM);
+let HTML = '';
+for (const item of ourProductNavData) {
+    HTML += `<div class="btn ourPr">${item.name}</div>`;
+}
+ourProductListDOM.innerHTML = HTML;
+
+/* our project: end */
 
 /* footer: start */
 /* footer: end */
