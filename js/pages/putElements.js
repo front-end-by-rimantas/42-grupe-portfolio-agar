d8577c396dbbbe91b17a6e45ae5f3068cfcd1853
Vytautas Valutis <dnc908@gmail.com>
2023-02-08 23:16:24 +0200

    Not JS responsibility

--- js/pages/putElements.js
+++ js/pages/putElements.js
@@ -2,10 +2,20 @@ import { ourProductNavData } from '../data/ourProductNavData.js';
 
 function putElements(num) {
     let HTML = ['', '', ''];
-    let colNmb = 3;
+    let colNmb = 0;
     let j = 0;
     let k = 0;
+
+    let width = document.defaultView.innerWidth;
     console.log('===========');
+    console.log(width);
+    if (width > 1200) {
+        colNmb = 3;
+    } else if (width > 768) {
+        colNmb = 2;
+    } else {
+        colNmb = 1;
+    }
     for (const item of ourProductNavData.tags[num - 1].projects) {
         k = j % colNmb;
         j++;
