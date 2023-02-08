a18692f763dc9c32d5f8101362c2820134a83d53
Vytautas Valutis <dnc908@gmail.com>
2023-02-08 13:47:35 +0200

    ourProduct nav bar

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,6 @@
 // IMPORT
 
-import { ourProductNavData } from '../data/ourProductNavData.js';
+// import { ourProductNavData } from '../data/ourProductNavData.js';
 import { ourProdClick } from '../pages/ourProdClick.js';
 
 // EXECUTION
@@ -10,13 +10,7 @@ import { ourProdClick } from '../pages/ourProdClick.js';
 
 /* our project: start */
 const ourProductListDOM = document.getElementById('our_product');
-let HTML = '';
-let idNum = 0;
 ourProdClick(1, ourProductListDOM);
-const num3 = document.getElementById('nmbr3');
-console.log(num3);
-num3.addEventListener('click', ourProdClick(3, ourProductListDOM));
-// document.getElementById('.nmbr3').onclick = ourProdClick(3, ourProductListDOM);
 
 /* our project: end */
 
--- js/pages/ourProdClick.js
+++ js/pages/ourProdClick.js
@@ -5,13 +5,20 @@ function ourProdClick(parm, place) {
     let idNum = 0;
     for (const item of ourProductNavData) {
         idNum++;
+        const a = 'nmbr' + idNum;
         if (idNum === parm) {
-            HTML += `<div id="nmbr${idNum}" class="btn ourPr invertOurPr">${item.name}</div>`;
+            HTML += `<div id="${a}" class="btn ourPr invertOurPr">${item.name}</div>`;
         } else {
-            HTML += `<div id="nmbr${idNum}" class="btn ourPr">${item.name}</div>`;
+            HTML += `<div id="${a}" class="btn ourPr">${item.name}</div>`;
         }
     }
     place.innerHTML = HTML;
+    for (let i = 1; i <= 6; i++) {
+        const a = 'nmbr' + i;
+        document.getElementById(a).addEventListener(`click`, () => {
+            ourProdClick(i, place);
+        });
+    }
 }
 
 export { ourProdClick };
