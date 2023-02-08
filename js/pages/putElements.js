782f79f245c54852c249a9a0c01dcfbd314177c0
Vytautas Valutis <dnc908@gmail.com>
2023-02-08 19:19:53 +0200

    Work w problem

--- js/pages/putElements.js
+++ js/pages/putElements.js
@@ -1,11 +1,22 @@
 import { ourProductNavData } from '../data/ourProductNavData.js';
 
 function putElements(num) {
-    let HTML = '';
+    let HTML = ['', '', ''];
+    let colNmb = 3;
+    let j = 0;
+    let k = 0;
     for (const item of ourProductNavData.tags[num - 1].projects) {
-        HTML += `<img class="ourPr-img col-12 col-md-6 col-xl-4" src="${ourProductNavData.path}${item}.jpg.webp" alt="Image">`;
+        k = (j % colNmb) + 1;
+        j++;
+        console.log(k, j, item);
+        HTML[k] += `<img class="ourPr-img" src="${ourProductNavData.path}${item}.jpg.webp" alt="Image">`;
     }
-    document.getElementById('productBox').innerHTML = HTML;
+    // const view = document.defaultView;
+    // console.log(view.innerWidth, view.innerHeight);
+    // console.log(HTML);
+    document.getElementById('productBox-1').innerHTML = HTML[1];
+    document.getElementById('productBox-2').innerHTML = HTML[2];
+    document.getElementById('productBox-3').innerHTML = HTML[3];
 }
 
 export { putElements };
