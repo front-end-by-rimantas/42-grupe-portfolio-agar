0eddc0205be993419cd82e8ddc2b56c8cd09faa6
Vytautas Valutis <dnc908@gmail.com>
2023-02-08 22:29:26 +0200

    Ice moved

--- js/pages/putElements.js
+++ js/pages/putElements.js
@@ -5,8 +5,9 @@ function putElements(num) {
     let colNmb = 3;
     let j = 0;
     let k = 0;
+    console.log('===========');
     for (const item of ourProductNavData.tags[num - 1].projects) {
-        k = (j % colNmb) + 1;
+        k = j % colNmb;
         j++;
         console.log(k, j, item);
         HTML[k] += `<img class="ourPr-img" src="${ourProductNavData.path}${item}.jpg.webp" alt="Image">`;
@@ -14,9 +15,9 @@ function putElements(num) {
     // const view = document.defaultView;
     // console.log(view.innerWidth, view.innerHeight);
     // console.log(HTML);
-    document.getElementById('productBox-1').innerHTML = HTML[1];
-    document.getElementById('productBox-2').innerHTML = HTML[2];
-    document.getElementById('productBox-3').innerHTML = HTML[3];
+    document.getElementById('productBox-1').innerHTML = HTML[0];
+    document.getElementById('productBox-2').innerHTML = HTML[1];
+    document.getElementById('productBox-3').innerHTML = HTML[2];
 }
 
 export { putElements };
