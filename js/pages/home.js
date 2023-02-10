e4c56da3c9f8ec4c063bed2cd9f5df7a07d2207e
Vytautas Valutis <dnc908@gmail.com>
2023-02-10 16:21:10 +0200

    End responsible & animations

--- js/pages/home.js
+++ js/pages/home.js
@@ -10,7 +10,7 @@ import { ourProdClick } from '../pages/ourProdClick.js';
 
 /* our project: start */
 const ourProductListDOM = document.getElementById('our_product');
-ourProdClick(1, ourProductListDOM);
+ourProdClick(0, ourProductListDOM);
 
 /* our project: end */
 
--- js/pages/ourProdClick.js
+++ js/pages/ourProdClick.js
@@ -5,17 +5,17 @@ function ourProdClick(parm, place) {
     let HTML = '';
     let idNum = 0;
     for (const item of ourProductNavData.tags) {
-        idNum++;
         const a = 'nmbr' + idNum;
         if (idNum === parm) {
             HTML += `<div id="${a}" class="btn ourPr invertOurPr">${item.name}</div>`;
         } else {
             HTML += `<div id="${a}" class="btn ourPr">${item.name}</div>`;
         }
+        idNum++;
     }
     place.innerHTML = HTML;
     putElements(parm);
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 0; i <= 5; i++) {
         const a = 'nmbr' + i;
         document.getElementById(a).addEventListener(`click`, () => {
             ourProdClick(i, place);
