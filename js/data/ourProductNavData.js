e5fcda915e4c997c5f98d2a1950892c7eb7dfce6
Vytautas Valutis <dnc908@gmail.com>
2023-02-10 10:35:55 +0200

    All zoomed img

--- js/data/ourProductNavData.js
+++ js/data/ourProductNavData.js
@@ -1,5 +1,5 @@
 const ourProductNavData = {
-    path: './img/OurProject/project-1-',
+    path: './img/OurProject/',
     tags: [
         {
             name: 'All',
@@ -24,6 +24,38 @@ const ourProductNavData = {
         {
             name: 'Agency',
             projects: [3, 6],
+        },
+    ],
+    projects: [
+        {
+            src: 'project-1-1',
+            srcZoom: 'project-zoom-1-1',
+            srcTxt: 'Graphics Design',
+        },
+        {
+            src: 'project-1-2',
+            srcZoom: 'project-zoom-1-2',
+            srcTxt: 'Markup Plan',
+        },
+        {
+            src: 'project-1-3',
+            srcZoom: 'project-zoom-1-3',
+            srcTxt: 'AI Development',
+        },
+        {
+            src: 'project-1-4',
+            srcZoom: 'project-zoom-1-4',
+            srcTxt: 'Social Media',
+        },
+        {
+            src: 'project-1-5',
+            srcZoom: 'project-zoom-1-5',
+            srcTxt: 'Partnership',
+        },
+        {
+            src: 'project-1-6',
+            srcZoom: 'project-zoom-1-6',
+            srcTxt: 'Esports Mirax',
         },
     ],
 };
