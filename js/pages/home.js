745a554b2fe6d67c018ff43fa9a3f9294d57bfe8
Katažina <katazina.fukalova@gmail.com>
2023-02-12 23:12:20 +0200

    back-to-top changes

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,16 +2,6 @@
 
 // EXECUTION
 /*back-to-top:start */
-const toTop = document.querySelector(`'.to-top'`);
-
-window.addEventListener(`'scroll'`, () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add('active'``);
-  } else {
-    toTop.classList.remove('active');
-  }
-})
-
 /*back-to-top:end */
 
 /* header: start */
