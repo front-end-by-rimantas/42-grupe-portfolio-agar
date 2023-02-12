5baabb7a513c2e9181f81ed095c15081b8b03358
Katažina <katazina.fukalova@gmail.com>
2023-02-12 22:19:54 +0200

    back-to-top

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,21 @@
 // IMPORT
 
 // EXECUTION
+/*back-to-top:start */
+<script type="text/javascript" >
+window.addEventListener('scroll', function(){
+    let scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle("active" , window.scrollY > 500px)
+})
+
+function scrollToTop (){
+    window.scrollTo({
+        top: 0
+    })
+}
+</script>
+
+/*back-to-top:end */
 
 /* header: start */
 /* header: end */
